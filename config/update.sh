b6e59f1a0d510822488589bdecc984a8ea667413
Demian <demiang@gmail.com>
2025-04-07 09:47:22 -0400

    Bug in update.sh
    Added permissions to all files

--- config/update.sh
+++ config/update.sh
@@ -7,6 +7,10 @@ source "/etc/global.config"
 date "+%Y/%m/%d   %H:%M:%S  PID:$$" >> "$LOG_STATUS"
 
 cd $1
+
+# get installation folder
+folder=`pwd`
+
 echo "Running gnss_station_manager update on `pwd`"
 git reset --hard
 git pull origin >> "$LOG_STATUS"
